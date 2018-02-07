0f294543ee7a09c11e2390293c31636f86b110ae
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2018-02-07 16:11:14 -0600

    starting chapter 9

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,12 +4,12 @@ var ctrlLocations = require('../controllers/locations');
 var ctrlOthers = require('../controllers/others');
 
 /* Locations pages */
-router.get('/', ctrlLocations.homelist);
 router.get('/location/:locationid', ctrlLocations.locationInfo);
 router.get('/location/:locationid/review/new', ctrlLocations.addReview);
 router.post('/location/:locationid/review/new', ctrlLocations.doAddReview);
 
 /* Other pages */
 router.get('/about', ctrlOthers.about);
+router.get('/', ctrlOthers.angularApp);
 
 module.exports = router;
