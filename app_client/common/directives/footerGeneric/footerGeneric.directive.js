e5ba50a9900504faf20c36c96b5e66442116a54d
Mohammad El-Sabae <mohammad.elsabae@gmail.com>
2021-02-10 22:52:05 -0600

    .

---
+++ app_client/common/directives/footerGeneric/footerGeneric.directive.js
@@ -0,0 +1,15 @@
+(function () {
+
+  angular
+    .module('loc8rApp')
+    .directive('footerGeneric', footerGeneric);
+
+  function footerGeneric () {
+    return {
+      restrict: 'EA',
+      templateUrl: '/common/directives/footerGeneric/footerGeneric.template.html'
+    };
+  }
+
+
+})();
